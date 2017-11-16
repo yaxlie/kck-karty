16777454e7103dfc3368571d0eb66b85b294fe49
Rareku <mariusz.nykaza@gmail.com>
2017-11-16 21:45:46 +0100

    wykrywanie asa dodane

--- preProc.py
+++ preProc.py
@@ -8,7 +8,7 @@ def preprocess_image(image):
 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 1)
-    mean = np.mean(blur * 2.5)
+    mean = np.mean(blur * 1)
     if(mean > 255):
         mean=255
     retval, thresh = cv2.threshold(blur, mean, 255, cv2.THRESH_BINARY)
@@ -37,13 +37,24 @@ def cutCard(image, card):
     
     warp = cv2.warpPerspective(card, M, (maxWidth, maxHeight))
     corner = warp[10:84, 10:32] 
-    mark = corner[3:45,:]
+    mark = corner[0:48,:]
+    print(mark.shape)
+    posBegin = np.float32([[0,5],[21,5],[21,45],[0,45]])
+    posEnd = np.array([[0,0],[75-1,0],[75-1,125-1],[0, 125-1]], np.float32)
+    M = cv2.getPerspectiveTransform(posBegin,posEnd)
+    mark = cv2.warpPerspective(mark, M, (75, 125))
+    mark = mark[0:125,0:75]
+    mark = preprocess_image(mark);
     cv2.imshow("debug",mark)
-    #findCards(mark)
-    return mark
+    return findCards(mark)
+    
 
 
 
 def findCards(mark):
-    img = cv2.imread("Card_Imgs/Ace.jpg",0)
-    cv2.imshow('imaggge',img)
+    machCard = 100000;
+    img = cv2.imread("A.png",0)
+    marged = cv2.absdiff(mark, img)
+    rank_diff = int(np.sum(marged)/255)
+    print(rank_diff)
+    return "Ace"
